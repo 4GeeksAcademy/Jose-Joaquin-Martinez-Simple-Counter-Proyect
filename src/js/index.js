332051d3f021e163972fe7281c8d12e0dc69858c
JoseJoaquinMartinez <61237112+JoseJoaquinMartinez@users.noreply.github.com>
2024-02-24 08:17:02 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,58 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import clock from "../img/clock.png";
 
-//import your own components
-import Home from "./component/home.jsx";
+let counter = 0;
+let countingInterval;
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const stopCount = () => {
+  clearInterval(countingInterval);
+};
+
+const restartCount = () => {
+  counter = 0;
+  renderCounter();
+};
+
+const resumeCount = () => {
+  countingInterval = setInterval(() => {
+    counter++;
+    renderCounter();
+  }, 1000);
+};
+
+const keyDownEvent = (e) => {
+  if (e.key === "Enter") {
+    let input = parseInt(e.target.value);
+    counter = input;
+    renderCounter();
+  }
+};
+
+const renderCounter = () => {
+  ReactDOM.render(
+    <div>
+      <div className="counter-container">
+        <div>
+          <img src={clock} alt="clock imagen" />
+        </div>
+        <div className="number sixth">{Math.floor(counter / 100000) % 10}</div>
+        <div className="number fifth">{Math.floor(counter / 10000) % 10}</div>
+        <div className="number fourth">{Math.floor(counter / 1000) % 10}</div>
+        <div className="number third">{Math.floor(counter / 100) % 10}</div>
+        <div className="number second">{Math.floor(counter / 10) % 10}</div>
+        <div className="number first">{Math.floor(counter / 1) % 10}</div>
+      </div>
+      <div>
+        <input type="number" onKeyDown={keyDownEvent}></input>
+        <button onClick={resumeCount}>Resume</button>
+        <button onClick={restartCount}>Restart</button>
+        <button onClick={stopCount}>Stop</button>
+      </div>
+    </div>,
+    document.querySelector("#app")
+  );
+};
+
+resumeCount();
